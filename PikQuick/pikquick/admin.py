3a6c376792cbbaa3ee75c75fa893b2e11586c792
Gaston1997 <fortuna_gaston@hotmail.com>
2015-10-19 11:48:37 -0300

    Detalles por hacer texto Imagen (models.py)

--- PikQuick/pikquick/admin.py
+++ PikQuick/pikquick/admin.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from pikquick.models import Entrada, Coment, Imagen
+from pikquick.models import Entrada, Coment #, Imagen
 
 # Register your models here.
 admin.site.register(Entrada)
 admin.site.register(Coment)
-admin.site.register(Imagen)
+#admin.site.register(Imagen)
