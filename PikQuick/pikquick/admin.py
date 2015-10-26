6b5c523e018d44e4b8e49771f6191be682212f7b
Mateo bossio <mateofede@hotmail.com>
2015-10-26 10:18:54 -0300

    borre la carpeta molesta

--- PikQuick/pikquick/admin.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.contrib import admin
-from pikquick.models import Entrada, Coment #, Imagen
-
-# Register your models here.
-admin.site.register(Entrada)
-admin.site.register(Coment)
-#admin.site.register(Imagen)
