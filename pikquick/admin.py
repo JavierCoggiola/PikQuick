6b95e3484f8665518227fd3e9573872c3f00de4d
JavierCoggiola <javi_cj_98@hotmail.com>
2015-11-06 11:15:00 -0300

    union boton seguir + lo otro

--- pikquick/admin.py
+++ pikquick/admin.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from pikquick.models import Entrada, Coment, Follow #, Imagen
+from pikquick.models import Entrada, Coment, Follow , Imagen
 
 # Register your models here.
 admin.site.register(Entrada)
 admin.site.register(Coment)
 admin.site.register(Follow)
-#admin.site.register(Imagen)
+admin.site.register(Imagen)
--- pikquick/models.py
+++ pikquick/models.py
@@ -10,26 +10,27 @@ class Entrada(models.Model):
 
     usuario = models.CharField(u'Usuario', max_length = 100)
     fecha = models.DateTimeField(u'Fecha del Post',auto_now_add=True)
-    img1 = models.FileField(u'Imagen de portada',upload_to = 'img_public', default='null')
-    img2 = models.FileField(u'Imagen de portada',upload_to = 'img_public', default='null')
-    desc1 = models.TextField(u'Descripcion Imagen 1' , max_length = 100 , default='')
-    desc2 = models.TextField(u'Descripcion Imagen 2' , max_length = 100 , default='')
-    descPub = models.TextField(u'Descripcion de la Publicacion' , max_length = 100 , default='Help')
+    #img1 = models.FileField(u'Imagen de portada',upload_to = 'img_public', default='null')
+    #img2 = models.FileField(u'Imagen de portada',upload_to = 'img_public', default='null')
+    #desc1 = models.TextField(u'Descripcion Imagen 1' , max_length = 100 , default='')
+    #desc2 = models.TextField(u'Descripcion Imagen 2' , max_length = 100 , default='')
+    descPub = models.TextField(u'Descripcion de la Publicacion' , max_length = 100 , default=' ')
 
     def __str__(self):
         return self.usuario.encode('utf8')+" / "+self.descPub.encode('utf8')
 
-#class Imagen (models.Model):
-#    class Meta:
-#        verbose_name = "Imagen"
-#        verbose_name_plural = "Imagenes"
-#
-#    img = models.FileField(u'Imagen de portada',upload_to = 'img_public', default='null')
-#    desc = models.TextField(u'Descripcion Imagen' , max_length = 100 , default=' ')
-#    entrada = models.ForeignKey(Entrada)
-#
-#    def __str__(self):
-#       return self.descPub.encode('utf8')
+class Imagen (models.Model):
+    class Meta:
+        verbose_name = "Imagen"
+        verbose_name_plural = "Imagenes"
+
+    img = models.FileField(u'Imagen de portada',upload_to = 'img_public', default='null')
+    desc = models.TextField(u'Descripcion Imagen' , max_length = 100 , default=' ')
+    entrada = models.ForeignKey(Entrada, related_name="imagenes")
+
+    def __str__(self):
+        return self.desc.encode('utf8')
+
 
 class Coment(models.Model):
     usuario = models.CharField(u'Usuario', max_length = 100, default=' ')
